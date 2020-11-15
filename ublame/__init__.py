90e286119ac29090f54115bc5a908f1488d0d98b
Fabrice Laporte <fabrice@yescapa.com>
2020-11-15 10:25:49 +0100

    feat: accept multiple patterns as input

--- ublame/__init__.py
+++ ublame/__init__.py
@@ -38,13 +38,13 @@ def repo_path_for(filename):
             return filename
 
 
-def trim_diff(diff, token, context=3):
+def trim_diff(diff, patterns, context=3):
     """Keep only context surrounding searched token."""
-    if token in diff:
+    if any([x in diff for x in patterns]):
         LOC_BEFORE = LOC_AFTER = context
         lines = diff.split("\n")
         for index, line in enumerate(lines):
-            if token in line:
+            if any([x in line for x in patterns]):
                 break
         if lines[index][0] not in ("+", "-"):
             return ""
@@ -52,48 +52,36 @@ def trim_diff(diff, token, context=3):
     return ""
 
 
-def diff_commit(commit, token):
+def diff_commit(commit, patterns):
     """Print diffs that contain token.
     Return True if token is present in file.
     """
     diffs = []
-    found = False
     for m in commit.modifications:
-        diff = trim_diff(m.diff, token)
+        diff = trim_diff(m.diff, patterns)
         if diff:
             diffs.append(diff)
-        elif m.source_code:
-            found |= token in m.source_code
 
     if any(diffs):
         print_commit_infos(commit)
         print(term.on_white(term.black("\n" + "\n".join(diffs))))
 
-    return any(diffs) or found
-
 
 @click.command(
-    context_settings=dict(help_option_names=["-h", "--help"]), help="Recursive blame",
+    context_settings=dict(help_option_names=["-h", "--help"]),
+    help="Search for PATTERNS in FILE commits history",
 )
 @click.argument("filename", type=click.Path(exists=True), metavar="FILE", nargs=1)
-@click.argument("token", nargs=1)
-def ublame_cli(filename, token):
+@click.argument("patterns", nargs=-1, required=True)
+def ublame_cli(filename, patterns):
     filename = os.path.abspath(filename)
     repo_path = repo_path_for(filename)
     relative_filename = filename.split(repo_path)[-1].strip("/")
     repo = GitRepository(repo_path)
-    ever_found = False
 
     for commit_hash in repo.get_commits_modified_file(relative_filename):
         commit = repo.get_commit(commit_hash)
-        found = diff_commit(commit, token)
-        ever_found |= found
-
-        if ever_found and not found:
-            exit()  # we went past the appearance of string in code, exit
-
-    if not ever_found:
-        print("'{}' not found in {} commits history".format(token, filename))
+        diff_commit(commit, patterns)
 
 
 if __name__ == "__main__":
